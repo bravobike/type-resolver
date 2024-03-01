f73327dc0834cda274fdd58a3389c1a24e5e9033
Simon Härer <simon.haerer@bravobike.de>
2024-03-01 11:59:45 +0100

    Move test files to support folder in test

--- mix.exs
+++ mix.exs
@@ -6,25 +6,22 @@ defmodule TypeResolver.MixProject do
       app: :type_resolver,
       version: "0.1.0",
       elixir: "~> 1.15",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
+  defp elixirc_paths(:test), do: ["lib", "test/support", "test/resources"]
+  defp elixirc_paths(_), do: ["lib"]
+
   def application do
-    [
-      extra_applications: [:logger, :ex_doc]
-    ]
+    []
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.31.1"},
       {:typed_struct, "~> 0.3.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end
