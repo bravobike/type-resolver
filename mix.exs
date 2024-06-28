08ec5947349815e4f5813a677db8882010a0d627
Simon Härer <simon.haerer@bravobike.de>
2024-06-28 13:55:06 +0200

    prepare mix.exs for hex release

--- mix.exs
+++ mix.exs
@@ -1,14 +1,25 @@
 defmodule TypeResolver.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @github_page "https://github.com/bravobike/type-resolver"
+
   def project do
     [
       app: :type_resolver,
-      version: "0.1.0",
-      elixir: "~> 1.15",
+      version: @version,
+      elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # doc
+      name: "TypeResolver",
+      description: "Parse and resolve spec types into convenient structs",
+      homepage_url: @github_page,
+      source_url: @github_page,
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -21,7 +32,29 @@ defmodule TypeResolver.MixProject do
 
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:typed_struct, "~> 0.3.0"}
+    ]
+  end
+
+  defp docs() do
+    [
+      api_reference: false,
+      authors: ["Simon Härer"],
+      canonical: "http://hexdocs.pm/type-resolver",
+      main: "TypeResolver",
+      source_ref: "v#{@version}"
+    ]
+  end
+
+  defp package do
+    [
+      files: ~w(mix.exs README.md lib),
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => @github_page
+      },
+      maintainers: ["Simon Härer"]
     ]
   end
 end
